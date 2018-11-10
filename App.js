67310734f3c346acbe5978f9f7c649e561e3a50a
fvonhoven <frank@infinite.red>
2018-11-10 01:24:04 -0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react"
+import { StackNavigator } from "react-navigation" // 1.0.0-beta.19
+import { ApolloClient } from "apollo-client"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { HttpLink } from "apollo-link-http"
+import { ApolloLink } from "apollo-link"
+import { ApolloProvider } from "react-apollo"
+import { withClientState } from "apollo-link-state"
+import { setContext } from "apollo-link-context"
+import { signIn, signOut, getToken } from "./util"
+
+import Register from "./Register"
+import Login from "./Login"
+import Profile from "./Profile"
+
+const AuthStack = StackNavigator({
+  Register: { screen: Register, navigationOptions: { headerTitle: "Register" } },
+  Login: { screen: Login, navigationOptions: { headerTitle: "Login" } }
+})
+
+const cache = new InMemoryCache()
+const stateLink = withClientState({ cache })
+const httpLink = new HttpLink({ uri: "https://494l130kz9.lp.gql.zone/graphql" })
+const authLink = setContext(async (req, { headers }) => {
+  const token = await getToken()
+  return {
+    ...headers,
+    headers: {
+      authorization: token ? `Bearer ${token}` : null
+    }
+  }
+})
+const link = authLink.concat(httpLink)
+const client = new ApolloClient({
+  cache,
+  link: ApolloLink.from([stateLink, link])
+})
+
+const LoggedInStack = StackNavigator({
+  Profile: { screen: Profile, navigationOptions: { headerTitle: "Profile" } }
+})
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loggedIn: false
+    }
+  }
+
+  handleChangeLoginState = (loggedIn = false, jwt) => {
+    this.setState({ loggedIn })
+    if (loggedIn) {
+      signIn(jwt)
+    } else {
+      signOut()
+    }
+  }
+
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        {this.state.loggedIn ? (
+          <LoggedInStack screenProps={{ changeLoginState: this.handleChangeLoginState }} />
+        ) : (
+          <AuthStack screenProps={{ changeLoginState: this.handleChangeLoginState }} />
+        )}
+      </ApolloProvider>
+    )
+  }
+}
---
+++ Profile.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { Container, Text, Button, Content } from "native-base"
+import gql from "graphql-tag"
+import { graphql } from "react-apollo"
+import { View } from "react-native"
+
+class Logout extends React.Component {
+  handleLogout = () => {
+    return this.props.screenProps.changeLoginState(false)
+  }
+
+  render() {
+    const { currentUser } = this.props.data
+    return (
+      <Container>
+        <Content>
+          {currentUser && (
+            <View>
+              <Text>{currentUser._id}</Text>
+              <Text>{currentUser.email}</Text>
+            </View>
+          )}
+          <Button full onPress={this.handleLogout}>
+            <Text>Log Out</Text>
+          </Button>
+        </Content>
+      </Container>
+    )
+  }
+}
+
+export default graphql(
+  gql`
+    query User {
+      currentUser {
+        _id
+        email
+      }
+    }
+  `
+)(Logout)
